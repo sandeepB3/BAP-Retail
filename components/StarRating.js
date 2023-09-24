cc776516a17d25f5f69c50d3ab8396088c7bfcdd
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2023-09-24 20:23:22 +0530

    first commit

---
+++ components/StarRating.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+const StarRating = ({ maxStars, rating }) => {
+  const renderStar = (index) => {
+    const filled = index <= rating;
+    const starIcon = filled ? (
+      <AntDesign key={index} name="star" size={12} color="#dc6920" />
+    ) : (
+      <AntDesign key={index} name="staro" size={12} color="#dc6920" />
+    );
+
+    return starIcon;
+  };
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {Array.from({ length: maxStars }, (_, index) => renderStar(index + 1))}
+    </View>
+  );  
+};
+
+export default StarRating;
---
+++ response.js
@@ -0,0 +1,227 @@
+export const response = [
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "retail-bpp-infra2.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://retail-bpp-infra2.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "HBO"
+                },
+                "id": "./retail.kirana/ind.blr/0@retail-bpp-infra2.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "retail-osm-stage.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://retail-osm-stage.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "BuyZilla"
+                },
+                "id": "./retail.kirana/ind.blr/0@retail-osm-stage.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "retail-osm-prod.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://retail-osm-prod.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "ShopNotch"
+                },
+                "id": "./retail.kirana/ind.blr/0@retail-osm-prod.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "tourism-bpp-infra1.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://tourism-bpp-infra1.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "HBO"
+                },
+                "id": "./retail.kirana/ind.blr/0@tourism-bpp-infra1.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "retail-bpp-infra1.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://retail-bpp-infra1.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "HBO"
+                },
+                "id": "./retail.kirana/ind.blr/0@retail-bpp-infra1.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "tourism-bpp-infra2.becknprotocol.io",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://tourism-bpp-infra2.becknprotocol.io//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "HBO"
+                },
+                "id": "./retail.kirana/ind.blr/0@tourism-bpp-infra2.becknprotocol.io"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "mandi.succinct.in",
+            "city": "std:080",
+            "max_callbacks": 1,
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "core_version": "0.9.2",
+            "ttl": "PT10M",
+            "bap_id": "sandeep-retail",
+            "domain": "nic2004:52110",
+            "bpp_uri": "https://mandi.succinct.in//boc/bpp",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z"
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/descriptor": {
+                    "name": "MANDI"
+                },
+                "id": "./0@mandi.succinct.in"
+            }
+        }
+    },
+    {
+        "context": {
+            "transaction_id": "1209849124",
+            "country": "IND",
+            "bpp_id": "bocshopify.humbhionline.in",
+            "city": "std:080",
+            "message_id": "dc106889-673a-4156-b2cf-34b3debff19f",
+            "ttl": "PT10M",
+            "core_version": "0.9.2",
+            "bap_id": "sandeep-retail",
+            "bpp_uri": "https://bocshopify.humbhionline.in/bpp",
+            "domain": "nic2004:52110",
+            "action": "on_search",
+            "bap_uri": "https://sandeep-bap.loca.lt",
+            "timestamp": "2023-09-03T17:34:36.049Z",
+            "max_callbacks": 1
+        },
+        "message": {
+            "catalog": {
+                "bpp/providers": [],
+                "bpp/fulfillments": [],
+                "bpp/descriptor": {
+                    "symbol": "https://cdn.shopify.com/s/files/1/0707/2808/9889/files/512x512.png?v=1676631279",
+                    "images": [
+                        "https://cdn.shopify.com/s/files/1/0707/2808/9889/files/512x512.png?v=1676631279"
+                    ],
+                    "code": "bocshopify.humbhionline.in",
+                    "name": "Test shopify store",
+                    "short_desc": "Test shopify store",
+                    "long_desc": "Test shopify store"
+                }
+            }
+        }
+    }
+];
+
+export const products = [{"context": {"action": "on_search", "bap_id": "sandeep-retail", "bap_uri": "https://sandeep-bap.loca.lt", "bpp_id": "retail-osm-prod.becknprotocol.io", "bpp_uri": "https://retail-osm-prod.becknprotocol.io//boc/bpp", "city": "std:080", "core_version": "0.9.2", "country": "IND", "domain": "nic2004:52110", "max_callbacks": 1, "message_id": "61c2f7c6-11c6-492f-856a-fb062e7c2858", "timestamp": "2023-09-23T08:01:59.550Z", "transaction_id": "1c369c00-5f5b-4f43-aedc-25ba6df76f91", "ttl": "PT10M"}, "message": {"catalog": {} }}]
